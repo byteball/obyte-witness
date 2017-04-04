14177d61873b2e902bde67c19a0c35d2f8eb902b
tony <tonyofbyteball@users.noreply.github.com>
2017-04-04 16:44:03 +0300

    default threshold 20

--- conf.js
+++ conf.js
@@ -16,7 +16,7 @@ exports.control_addresses = ['DEVICE ALLOWED TO CHAT'];
 exports.payout_address = 'WHERE THE MONEY CAN BE SENT TO';
 
 exports.bSingleAddress = true;
-exports.THRESHOLD_DISTANCE = 10;
+exports.THRESHOLD_DISTANCE = 20;
 exports.MIN_AVAILABLE_WITNESSINGS = 100;
 
 exports.KEYS_FILENAME = 'keys.json';
