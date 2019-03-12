dc37841bef77c55856d50092ac41ecc60c4c7673
tony <tonyofbyteball@users.noreply.github.com>
2019-03-12 03:06:42 +0300

    testnet via .env

--- conf.js
+++ conf.js
@@ -9,7 +9,7 @@ exports.bLight = false;
 exports.storage = 'sqlite';
 
 
-exports.hub = 'obyte.org/bb';
+exports.hub = process.env.testnet ? 'obyte.org/bb-test' : 'obyte.org/bb';
 exports.deviceName = 'Witness';
 exports.permanent_pairing_secret = 'randomstring';
 exports.control_addresses = ['DEVICE ALLOWED TO CHAT'];
